3d4ef1725f5a627a7bf6e62b09137b23e33ec3ce
anorisno <anorisno@anorisno.anorisno>
2023-11-16 19:16:51 +0300

    new file:   resources/static/elm.js 	new file:   resources/static/index1.html 	modified:   src/sale_adviser_clojure/core.clj

--- src/sale_adviser_clojure/core.clj
+++ src/sale_adviser_clojure/core.clj
@@ -1,19 +1,25 @@
 (ns sale-adviser-clojure.core
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :as route]
-            [ring.adapter.jetty :as jetty])
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :refer [file-response]])
   (:gen-class))
 
 (defroutes app-routes
            (GET "/" [] "Hello, World!")
            (route/not-found "Not Found"))
 
-(defn hendler [request]
+(defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
 
+(defn handler2 [request]
+  (file-response "index1.html" {:root "static"}))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (jetty/run-jetty handler2  {:port 3000 :join? false}))
