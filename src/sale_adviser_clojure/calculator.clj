68dd58776984d857a82afdcbd4cabb387e40d668
anorisno <anorisno@anorisno.anorisno>
2024-01-19 09:16:07 +0300

    add some document

--- src/sale_adviser_clojure/calculator.clj
+++ src/sale_adviser_clojure/calculator.clj
@@ -9,19 +9,19 @@
   [coll]
   (let [last-record (last coll)
         before-last-record (last (butlast coll))]
-    (/ (- (:quantity before-last-record) ( :quantity last-record)) (- (.toEpochSecond  (:sale-date before-last-record) (java.time.ZoneOffset/UTC)) (.toEpochSecond (:sale-date last-record) (java.time.ZoneOffset/UTC))))))
+    (/ (- (:quantity before-last-record)
+          ( :quantity last-record))
+       (- (.toEpochSecond  (:sale-date before-last-record) (java.time.ZoneOffset/UTC))
+          (.toEpochSecond (:sale-date last-record) (java.time.ZoneOffset/UTC))))))
     ;(- (before-last-record :quantity) (last-record :quantity))(/  (- (before-last-record :date) (last-record :date)))))
 
 (defn calculate-prediction
   [product-id prediction-date]
   (let [sales (sale/get-all-sale-by-product-id-order-by-date product-id)]
-    (+
-      (:quantity (last sales))
-      (*
-        (derivative-of-last-two sales)
-        (-
-          (.toEpochSecond prediction-date (java.time.ZoneOffset/UTC))
-          (.toEpochSecond (:sale-date (last sales)) (java.time.ZoneOffset/UTC)))))))
+    (+ (:quantity (last sales))
+       (* (derivative-of-last-two sales)
+          (- (.toEpochSecond prediction-date (java.time.ZoneOffset/UTC))
+             (.toEpochSecond (:sale-date (last sales)) (java.time.ZoneOffset/UTC)))))))
 
 (defn get-prediction
   [product-id prediction-date]
